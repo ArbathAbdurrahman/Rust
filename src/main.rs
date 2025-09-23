ca3f3bd96a623d675556c98ff44b09fc59af7d00
Arbath Abdurrahman <arbath@teknohole.com>
2025-09-23 20:48:51 +0700

    Float, Integer, Konversi

--- src/main.rs
+++ src/main.rs
@@ -40,3 +40,51 @@ fn shadowing(){
     println!("Hello, {}!", nama);
 }
 
+/*
+Tipe data rust:
+Scalar = nilai tunggal
+Compound = bisa lebih dari satu
+ */
+
+// TIPE DATA SCALAR
+#[test]
+fn integer(){
+    // rust sudah otomatis mendeteksi tipe data seperti python tetapi bisa juga secara eksplisit sebutkan tipe data
+    let implisit = 12;
+    let eksplisit: i16 = -12; // ini integer berukuran 16 bit
+    let unsignd: u32 = 24; // ini hanya menerima bilangan  positif
+
+    println!("implisit: {}", implisit);
+    println!("eksplisit: {}", eksplisit);
+    println!("unsignd: {}", unsignd);
+}
+
+#[test]
+fn floating(){
+    // float diawali dengan fbit dan hanya 32 dan 64 bit
+    let implisit = 12.34;
+    let eksplisit: f32 = -123.45;
+
+    println!("implisit: {}", implisit);
+    println!("eksplisit: {}", eksplisit);
+}
+
+#[test]
+fn konversi(){
+    // kita bisa melakukan konversi tipe data asalkan masih muat dengan 'as'
+    let a: i8 = 10;
+    println!("a: {}", a);
+
+    let b: i16 = a as i16;
+    println!("b: {}", b);
+
+    let c: i32 = b as i32;
+    println!("c: {}", c);
+
+    // kalau tidak muat maka akan terjadi integer overflow
+    let d: i64 = 1000000000;
+    println!("d: {}", d);
+
+    let e: i16 = d as i16;
+    println!("e: {}", e); // akan tidak sesuai keinginan
+}
